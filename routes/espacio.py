ef1e53e5948bd2a1c366cefece2624f33ea4d045
CarolinaMejiaMujica <carolinae.mejiam@pucp.edu.pe>
2021-09-08 21:27:00 -0500

    graficos listos

--- routes/espacio.py
+++ routes/espacio.py
@@ -30,47 +30,76 @@ from bokeh.transform import cumsum
 
 espacio = APIRouter()
 
-@espacio.get("/mapa")
-def grafico():
+@espacio.get("/mapa/")
+def grafico(fechaIni: str,fechaFin: str):
     df_departamentos=pd.DataFrame(conn.execute(departamentos.select()).fetchall())
     df_departamentos.columns=['ID', 'Nombre', 'latitud', 'longitud']
-    df_dep=pd.DataFrame(conn.execute(f"select count(s.id_secuencia) AS count, d.nombre from departamentos as d LEFT JOIN secuencias as s ON d.id_departamento=s.id_departamento LEFT JOIN agrupamiento as a ON s.id_secuencia=a.id_secuencia LEFT JOIN variantes as v ON a.id_variante=v.id_variante LEFT JOIN modelos as m ON a.id_modelo=m.id_modelo and m.nombre like 'k-means' group by d.nombre order by d.nombre").fetchall())
+
+    df_dep=pd.DataFrame(conn.execute(f"select count(s.id_secuencia) AS count, d.nombre from departamentos as d "+
+                                 "LEFT JOIN secuencias as s ON d.id_departamento=s.id_departamento "+
+                                 "LEFT JOIN agrupamiento as a ON s.id_secuencia=a.id_secuencia "+
+                                 "LEFT JOIN variantes as v ON a.id_variante=v.id_variante "+
+                                 "LEFT JOIN algoritmos as m ON a.id_algoritmo=m.id_algoritmo "+
+                                 "where m.nombre like 'k-means' and m.parametro=10 and "+
+                                 "s.fecha_recoleccion >= \'"+ fechaIni +"\' and s.fecha_recoleccion<=\'"+ fechaFin +"\' "+
+                                 "group by d.nombre order by d.nombre").fetchall())
     df_dep.columns=['count','Nombre']
-    df_departamentos = pd.merge(df_departamentos, df_dep, on='Nombre')
-    df_departamentos["variantes"] = ""
-    df_departamentos["variante"] = ""
-    df_vari=pd.DataFrame(conn.execute(f"SELECT d.nombre, COALESCE(v.id_variante,0) as id_variante, count(a.*), COALESCE(v.nombre,'') as nombre_variante, v.color from departamentos as d LEFT JOIN secuencias as s ON d.id_departamento=s.id_departamento LEFT JOIN agrupamiento as a ON s.id_secuencia=a.id_secuencia LEFT JOIN variantes as v ON a.id_variante=v.id_variante LEFT JOIN modelos as m ON a.id_modelo=m.id_modelo and m.nombre like 'k-means' GROUP BY d.nombre,v.id_variante ORDER BY d.nombre ASC").fetchall())
+    df_departamentos=df_departamentos.merge(df_dep, how='left', on='Nombre')
+    df_departamentos["variantes"] = "a"
+    df_departamentos["variante"] = "a"
+    df_departamentos['color']="a"
+    df_departamentos.loc[df_departamentos['count'].isnull(),'count']=0
+
+    df_vari=pd.DataFrame(conn.execute(f"SELECT d.nombre, COALESCE(v.id_variante,0) as id_variante, count(a.*), COALESCE(v.nombre,'') as nombre_variante, "+
+                                  "v.color from departamentos as d LEFT JOIN secuencias as s ON d.id_departamento=s.id_departamento "+
+                                  "LEFT JOIN agrupamiento as a ON s.id_secuencia=a.id_secuencia LEFT JOIN variantes as v ON a.id_variante=v.id_variante "+
+                                  "LEFT JOIN algoritmos as m ON a.id_algoritmo=m.id_algoritmo "+
+                                  "where m.nombre like 'k-means' and m.parametro=10 and "+
+                                 "s.fecha_recoleccion >= \'"+ fechaIni +"\' and s.fecha_recoleccion<=\'"+ fechaFin +"\' "+
+                                  "GROUP BY d.nombre,v.id_variante ORDER BY d.nombre ASC").fetchall())
     df_vari.columns=['Nombre','id_variante','count_variante','nombre_variante','color']
-    df_departamentos['color']=''
+    lista=set(df_vari['Nombre'])
+
     for i,d in enumerate(df_departamentos['Nombre']):
-        var_dep=list((df_vari.loc[df_vari['Nombre']==d]['nombre_variante']).unique())
-        variante_pred=(df_vari.loc[df_vari['Nombre']==d].max())['nombre_variante']
-        if len(variante_pred)==0:
-            df_departamentos['variantes'][i]='No hay datos'
-            df_departamentos['variante'][i]='No hay datos'
-            df_departamentos['color'][i]='#CDCDCD'
+        if d in lista:
+            var_dep=list((df_vari.loc[df_vari['Nombre']==d]['nombre_variante']).unique())
+            count_variante=df_vari.loc[df_vari['Nombre']==d]['count_variante'].max()
+            variante_pred=df_vari.loc[(df_vari['count_variante']==count_variante) & (df_vari['Nombre']==d)]['nombre_variante'].iloc[0]
+            if len(variante_pred)==0:
+                df_departamentos['variantes'].iloc[i]='No hay datos'
+                df_departamentos['variante'].iloc[i]='No hay datos'
+                df_departamentos['color'].iloc[i]='#CDCDCD'
+            else:
+                df_departamentos['variantes'].iloc[i]=var_dep
+                df_departamentos['variante'].iloc[i]=variante_pred
+                df_departamentos['color'].iloc[i]=list(df_vari.loc[df_vari['nombre_variante']==variante_pred]['color'])[0]
         else:
-            df_departamentos['variantes'][i]=var_dep
-            df_departamentos['variante'][i]=variante_pred
-            df_departamentos['color'][i]=list(df_vari.loc[df_vari['nombre_variante']==variante_pred]['color'])[0]
+            df_departamentos['variantes'].iloc[i]='No hay datos'
+            df_departamentos['variante'].iloc[i]='No hay datos'
+            df_departamentos['color'].iloc[i]='#CDCDCD'
+
+
+
     cjs = """
     console.log('Tap');
     console.log(source.selected.indices);
     var valor=source.selected.indices
     """
+    
     TOOLTIPS=[("Departamento", "@Nombre"),
-            ("Total de secuencias genómicas", "@count"),
-            ("Variantes identificadas","@variantes"),
-            ("Variante predominante","@variante"),
-            ("Color", "$variante $swatch:color")]
+          ("Total de secuencias genómicas", "@count"),
+          ("Variantes identificadas","@variantes"),
+          ("Variante predominante","@variante"),
+          ("Color", "$variante $swatch:color")]
 
-    fig = figure(tools="pan,zoom_in,zoom_out,undo,redo,reset,save",plot_width=700, plot_height=650,
-                x_axis_location=None, y_axis_location=None,tooltips=TOOLTIPS)
+    fig = figure(tools="pan,zoom_in,zoom_out,undo,redo,reset,save",plot_width=750, plot_height=600,
+                x_axis_location=None, y_axis_location=None,
+                tooltips=TOOLTIPS)
 
     fig.grid.grid_line_color = None
     valor=fig.patches("longitud", "latitud", source=df_departamentos,
                 fill_color={'field': 'variante', 'transform': CategoricalColorMapper(palette=df_departamentos['color'], factors=df_departamentos['variante'].to_list())},
-                fill_alpha=0.7, line_color="white", line_width=0.5,legend='variante')
+                fill_alpha=0.7, line_color="white", line_width=0.5,legend_group='variante')
 
     fig.legend.location = "bottom_left"
     fig.legend.title = 'Variantes'
@@ -87,4 +116,5 @@ def grafico():
     cb = CustomJS(args=dict(source=valor.data_source), code=cjs)
     ttool = TapTool(callback=cb)
     fig.tools.append(ttool)
+
     return json.dumps(json_item(fig, "mapa"))
--- routes/tiempo.py
+++ routes/tiempo.py
@@ -26,38 +26,54 @@ from bokeh.models import DatetimeTickFormatter
 from bokeh.models import HoverTool
 from math import pi
 from bokeh.transform import cumsum
+from pydantic import BaseModel
 
 tiempo = APIRouter()
 
-def data_secuencias():
-    df_secu=pd.DataFrame(conn.execute(f"select count(s.id_secuencia),s.fecha_recoleccion, v.nombre as variante, v.color from secuencias  as s LEFT JOIN agrupamiento as a ON s.id_secuencia=a.id_secuencia LEFT JOIN variantes as v ON a.id_variante=v.id_variante LEFT JOIN modelos as m ON a.id_modelo=m.id_modelo and m.nombre like 'k-means' group by s.fecha_recoleccion, v.nombre,v.color order by s.fecha_recoleccion").fetchall())
+def data_secuencias(ini,fin):
+    df_secu=pd.DataFrame(conn.execute(f"select count(s.id_secuencia),s.fecha_recoleccion, v.nombre as variante, v.color from secuencias as s "+
+            "LEFT JOIN agrupamiento as a ON s.id_secuencia=a.id_secuencia LEFT JOIN variantes as v ON a.id_variante=v.id_variante "+
+            "LEFT JOIN algoritmos as m ON a.id_algoritmo=m.id_algoritmo "+
+            "where m.nombre like 'k-means' and m.parametro=10 and "+
+            "s.fecha_recoleccion >= \'"+ ini +"\' and s.fecha_recoleccion<= \'"+ fin +
+            "\' group by s.fecha_recoleccion, v.nombre,v.color order by s.fecha_recoleccion").fetchall())
     df_secu.columns=['count','fecha','variante','color']
     return df_secu
 
-@tiempo.get("/graficolineal")
-def grafico():
-    df_secu=data_secuencias()
+@tiempo.post("/graficolineal/")
+def grafico(fechaIni: str,fechaFin: str):
+    df_secu=data_secuencias(fechaIni,fechaFin)
+    fechas=list(set(df_secu['fecha']))
     variant=list(set(df_secu['variante']))
+    for v in variant:
+        fechas_variante=list(set(df_secu.loc[df_secu['variante']==v]['fecha']))
+        for m in fechas:
+            if m not in fechas_variante:
+                df_secu=df_secu.append(pd.Series([0, m, v,df_secu.loc[df_secu['variante']==v]['color'].iloc[0]], index=df_secu.columns),ignore_index=True)
+
     colores=list()
     for a in list(set(df_secu['variante'])):
         colores.append(list(df_secu.color.loc[df_secu['variante']==a])[0])
 
-    p = figure(tools="zoom_in,zoom_out,undo,redo,reset,save",plot_width=1600, plot_height=500, x_axis_type="datetime")
+    p = figure(tools="pan,zoom_in,zoom_out,undo,redo,reset,save",plot_width=1650, plot_height=500, x_axis_type="datetime")
 
-    hover=HoverTool(tooltips=[('Variante','@variante'),('Fecha de recolección', '@fecha{%d-%m-%Y}'),("Cantidad de secuencias genómicas","@count")],formatters={'@fecha': 'datetime'})
+    hover=HoverTool(tooltips=[('Fecha de recolección', '@fecha{%d-%m-%Y}'),
+                            ("Cantidad de secuencias genómicas","@count"),
+                            ("Variante",'@variante'),
+                            ("Color","$variante $swatch:color")],formatters={'@fecha': 'datetime'})
     p.add_tools(hover)
 
     for name,color in zip(variant,colores):
-        df = pd.DataFrame(df_secu.loc[df_secu['variante']==name])
-        df.reset_index(inplace=True, drop=True)
-        p.line('fecha','count',source=df, line_width=2, color=color, alpha=0.8,muted_color=color, muted_alpha=0.2, legend_label=name)
+        df = pd.DataFrame(df_secu.loc[df_secu['variante']==name].sort_values('fecha')).reset_index(drop=True)
+        p.line('fecha','count',source=df, line_width=2, color=color, alpha=0.8,
+            muted_color=color, muted_alpha=0.2, legend_label=name)
 
     p.legend.location = "top_left"
     p.legend.title = 'Variantes'
     p.legend.title_text_font_style = "bold"
     p.legend.title_text_font_size = "15px"
     p.legend.label_text_font_size = '11pt'
-    p.legend.click_policy = "mute"
+    p.legend.click_policy = "hide"
     p.xaxis.axis_label = "Mes-Año"
     p.yaxis.axis_label="Número de secuencias genómicas"
     p.xaxis.axis_label_text_font_style = "bold"
@@ -68,22 +84,20 @@ def grafico():
     p.yaxis.major_label_text_font_style="bold"
     p.xaxis.major_label_text_font_size="12px"
     p.yaxis.major_label_text_font_size="12px"
-
-    p.xaxis.formatter=DatetimeTickFormatter(days=["%m-%Y"],months=["%m-%Y"],years=["%m-%Y"])
-
+    p.xaxis.formatter=DatetimeTickFormatter(days=["%d-%m-%Y"],months=["%d-%m-%Y"],years=["%d-%m-%Y"])
     return json.dumps(json_item(p, "graficolineal"))
 
 
-@tiempo.get("/graficocircular")
-def grafico():
-    df_secu=data_secuencias()
+@tiempo.get("/graficocircular/")
+def grafico(fechaIni: str,fechaFin: str):
+    df_secu=data_secuencias(fechaIni,fechaFin)
     data=pd.DataFrame(df_secu.groupby(by=["variante","color"]).sum()[["count"]])
     data.rename(columns={"variante":"variante","color":"color","count":"count"})
     data=data.reset_index()
     data['angulo'] = data['count']/data['count'].sum()*2*pi
     data['pocentaje'] = round(data['count']/data['count'].sum()*100,2)
 
-    p = figure(tools="pan,zoom_in,zoom_out,undo,redo,reset,save",plot_width=700, plot_height=600,
+    p = figure(tools="pan,zoom_in,zoom_out,undo,redo,reset,save",plot_width=750, plot_height=600,
             tooltips=[("Variante","@variante"),("Porcentaje","@pocentaje{1.11} %"), ("Cantidad","@count")])
 
     p.wedge(x=0, y=1, radius=0.8,
@@ -104,5 +118,4 @@ def grafico():
     p.legend.spacing = 0
     p.legend.padding = 1
     p.legend.margin = 5
-
     return json.dumps(json_item(p, "graficocircular"))
